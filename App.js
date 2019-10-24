03320a54f9701a6c36065bd83c8ec45cbcde3d13
Joe Anton <janton153@gmail.com>
2019-10-23 17:57:16 -0700

    Navigation setup

--- App.js
+++ App.js
@@ -1,19 +1,71 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Example from './screens/Example';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AuthStack = createStackNavigator({
+  Landing: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Landing',
+    },
+  },
+  SignIn: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Sign In',
+    },
+  },
+  CreateAccount: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Create Account',
+    },
+  },
+  ForgotPassword: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Forgot Password',
+    },
+  },
+  ResetPassword: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Reset Password',
+    },
   },
 });
+
+const App = createSwitchNavigator({
+  Loading: {
+    screen: Example,
+  },
+  Auth: {
+    screen: AuthStack,
+  },
+  App: {
+    screen: Example,
+  },
+});
+
+export default createAppContainer(App);
